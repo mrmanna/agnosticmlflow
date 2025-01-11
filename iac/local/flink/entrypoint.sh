71e1664098955ed9f80039152daca9babc3a6894
Manna Mahmud <mahmudur.r.manna@gmail.com>
2025-01-11 18:19:53 +0600

    Fixes issues #1,2 and 3 beam job can now read write from MinIO and can run in Flink with Sidecar

--- iac/local/flink/entrypoint.sh
+++ iac/local/flink/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Wait until Docker socket is available
-while ! docker ps > /dev/null 2>&1; do
-    echo "Waiting for Docker socket to be ready..."
-    sleep 1
-done
+# # Wait until Docker socket is available
+# while ! docker ps > /dev/null 2>&1; do
+#     echo "Waiting for Docker socket to be ready..."
+#     sleep 1
+# done
 
-echo "Docker socket is ready."
-
+# echo "Docker socket is ready."
+# /opt/apache/beam/boot &
 # Execute Flink process
 if [ "$1" = "jobmanager" ] || [ "$1" = "taskmanager" ]; then
-  exec /opt/flink/bin/"$1".sh start-foreground
+
+exec /opt/flink/bin/"$1".sh start-foreground
+
 else
   echo "Unknown command: $1"
   exit 1
--- services/customer-churn/data_ingestion_app/src/main.py
+++ services/customer-churn/data_ingestion_app/src/main.py
@@ -1,29 +1,40 @@
 import os
+
+import botocore
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import S3Options
 from minio import Minio
 from minio.error import S3Error
 import logging
 import glob
+import boto3
+from botocore.config import Config
 
+# from apache_beam.io.aws.s3filesystem import S3FileSystem
+# from apache_beam.io.filesystems import FileSystems
+# FileSystems.register(S3FileSystem())
+# os.environ["AWS_ACCESS_KEY_ID"] = ACCESS_KEY
+# os.environ["AWS_SECRET_ACCESS_KEY"] = SECRET_KEY
+# os.environ["AWS_REGION"] = "us-east-1"
+# os.environ["AWS_S3_ENDPOINT"] = MINIO_ENDPOINT
 # Setup Logging
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # MinIO Configuration
-MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "localhost:9000")
-ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
-SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
+MINIO_ENDPOINT = os.getenv("AWS_S3_ENDPOINT", "localhost:9000")
+ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID", "minioadmin")
+SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "minioadmin")
 RAW_BUCKET = "raw-data"
 PROCESSED_BUCKET = "processed-data"
 
 # MinIO Client Initialization
 client = Minio(
-    MINIO_ENDPOINT,
+    MINIO_ENDPOINT.replace("http://", "").replace("https://", ""),
     access_key=ACCESS_KEY,
     secret_key=SECRET_KEY,
     secure=False
 )
-
 def create_bucket(bucket_name):
     """Create bucket if it doesn't exist."""
     if not client.bucket_exists(bucket_name):
@@ -49,20 +60,44 @@ def preprocess_with_beam(input_file, output_file):
     """Process data using Apache Beam with SparkRunner."""
     # print("Starting preprocessing with Apache Beam (SparkRunner)...")
 
-    # # SparkRunner Pipeline Options
-    # pipeline_options = PipelineOptions(
-    #     runner='SparkRunner',  # Enable SparkRunner for distributed processing
-    #     job_name="data-preprocessing-job",
-    #     job_endpoint="beam-job-server:8099",  # Use Kubernetes service name
-    #  )
-    pipeline_options = PipelineOptions([  
-    "--runner=PortableRunner",
-    "--job_endpoint=beam-job-server:8099",
-    "--artifact_endpoint=beam-job-server:8098",    
-    "--environment_type=DOCKER",
-    "--environment_config=apache/beam_python3.8_sdk:2.55.1"
+    #DOCKER CONFIG
+    # pipeline_options = PipelineOptions([  
+    # "--runner=PortableRunner",
+    # "--job_endpoint=beam-job-server:8099",
+    # "--artifact_endpoint=beam-job-server:8098",    
+    # "--environment_type=DOCKER",
+    # "--environment_config=apache/beam_python3.8_sdk:2.55.1"
+    # ])
+    # #PROCESS CONFIG
+    #     # Define the JSON config in a variable
+    # env_config = '{"command": "/opt/apache/beam/boot"}'
+    # pipeline_options = PipelineOptions([
+    #     "--runner=PortableRunner",
+    #     "--job_endpoint=beam-job-server:8099",
+    #     "--artifact_endpoint=beam-job-server:8098",
+    #     "--environment_type=PROCESS",
+    #     f"--environment_config={env_config}",
+    #     "--no_validate"
+    # ])
+    #SIDECAR
+    env_config = '{"command": "/opt/apache/beam/boot"}'
+    pipeline_options = PipelineOptions([
+        "--runner=PortableRunner",
+        "--job_endpoint=beam-job-server:8099",
+        "--artifact_endpoint=beam-job-server:8098",
+        "--environment_type=EXTERNAL",
+        "--environment_config=localhost:50000",
+        "--requirements_file=./requirements.txt",
+        "--no_validate"
     ])
-    # print("Apache Beam (SparkRunner) Configured...")
+    
+      # Set S3 options
+    s3_options = pipeline_options.view_as(S3Options)
+    s3_options.s3_region_name = "us-east-1"
+    s3_options.s3_access_key_id = ACCESS_KEY
+    s3_options.s3_secret_access_key = SECRET_KEY
+    s3_options.s3_endpoint_url = MINIO_ENDPOINT
+
     # Validation and Transformation Function
     def validate_and_transform(row):
         """Validation and preprocessing logic for retail sales data."""
@@ -100,15 +135,21 @@ def preprocess_with_beam(input_file, output_file):
     with beam.Pipeline(options=pipeline_options) as p:
         (
             p
-            | "Read CSV" >> beam.io.ReadFromText(input_file, skip_header_lines=1)
+            | "Read CSV" >> beam.io.ReadFromText("s3://raw-data/sales_data_01.csv", skip_header_lines=1)
             | "Filter Rows" >> beam.Filter(lambda row: len(row.split(",")) >= 25)  # Flexible filter
             | "Validate and Transform" >> beam.Map(validate_and_transform)
-            | "Write Processed Data" >> beam.io.WriteToText(output_file, file_name_suffix=".csv")
+            | "Write Processed Data" >> beam.io.WriteToText("s3://processed-data/sales_data", file_name_suffix=".csv")
         )
+        # Simple pipeline definition
+    # with beam.Pipeline(options=pipeline_options) as p:
+    #     (
+    #         p
+    #         | "Create Input" >> beam.Create(["Hello, World!", "Apache Beam", "Simple Pipeline"])
+    #     )
     # print("Preprocessing complete.")
 
 def upload_to_minio(file_path, bucket_name):
-    """Upload processed data files to MinIO."""
+    """Upload data files to MinIO."""
     for file in glob.glob(file_path):  # Handle Apache Beam sharded outputs
         file_name = os.path.basename(file)
         client.fput_object(bucket_name, file_name, file)
@@ -122,7 +163,7 @@ def main():
 
         # Step 2: Download Sample Data
       #  raw_file = download_sample_data()
-        local_file = "sales_data.csv"
+        local_file = "sales_data_01.csv"
         upload_to_minio(local_file, RAW_BUCKET)
 
         # Step 3: Preprocess with Apache Beam and Spark
